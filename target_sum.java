6fa0c81ae725b58ec1aeaa947dd182580fc01c56
Felcia A <jeyakumararul91@gmail.com>
2026-03-25 11:40:55 +0530

    Add files via upload

---
+++ target_sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class target_sum {
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        a[i]=s.nextInt();
+        int k=s.nextInt();
+        for(int i=0;i<n-1;i++){
+            if(a[i]+a[i+1]==k){
+               System.out.print(i+" "+(i+1));
+               return;
+            }
+        }
+    }
+}
